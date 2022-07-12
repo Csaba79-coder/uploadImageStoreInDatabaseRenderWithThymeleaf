05fd5ba2a5add87f74da28a9ea896d48eda8cc5a
Csaba79-coder <csabavadasz79@gmail.com>
2022-07-12 18:01:48 +0200

    Initial commit

---
+++ src/main/java/com/csaba79coder/uploadimagestoreindatabaserenderwiththymeleaf/UploadImageStoreInDatabaseRenderWithThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.uploadimagestoreindatabaserenderwiththymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadImageStoreInDatabaseRenderWithThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadImageStoreInDatabaseRenderWithThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csaba79coder/uploadimagestoreindatabaserenderwiththymeleaf/UploadImageStoreInDatabaseRenderWithThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.uploadimagestoreindatabaserenderwiththymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadImageStoreInDatabaseRenderWithThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
